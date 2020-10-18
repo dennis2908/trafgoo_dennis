54b695667715cfc46240348b9a0fcb9fdfb1ca7b
dennis M <manullang_d@yahoo.com>
2020-10-18 09:51:28 +0700

    dmdm

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -11,6 +11,10 @@
 		width:auto;
 		border-radius: 4px;	
     }
+	.apecsaos-wrapper{
+		background-color:white;
+		
+	}
 </style>
 <div id="navbar-fixed-apecsaos">
         <nav class="navbar navbar-default">
@@ -110,48 +114,9 @@
               slideup
               slidedown
               fade -->
-              <li
-                data-transition="slotzoom-horizontal"
-                data-slotamount="3"
-                data-masterspeed="3000"
-                data-thumb="{{asset('assets/images/webs1.jpg')}}"
-                data-saveperformance="off"
-                data-title="@lang('sentences.contact_us')">
-                <img
-                  src="{{asset('assets/images/payincrease.jpg')}}"
-                  alt=""
-                  data-bgposition="center top"
-                  data-bgfit="cover"
-                  data-bgrepeat="no-repeat" style="size:30%">
-              </li>
-
-              <li
-                data-transition="curtain-3"
-                data-slotamount="3"
-                data-masterspeed="3000"
-                data-thumb="{{asset('assets/images/webs1.jpg')}}"
-                data-saveperformance="off"
-                data-title="@lang('sentences.with_awesome_service')">
-                <img
-                  src="{{asset('assets/images/payincrease.jpg')}}"
-                  alt=""
-                  data-bgposition="center top"
-                  data-bgfit="cover"
-                  data-bgrepeat="no-repeat" style="size:30%">
-              </li>
-              <li
-                data-transition="slideright"
-                data-slotamount="3"
-                data-masterspeed="3000"
-                data-thumb="{{asset('assets/images/webs1.jpg')}}"
-                data-saveperformance="off"
-                data-title="@lang('sentences.we_are_awesome')">
-                <img
-                  src="{{asset('assets/images/payincrease.jpg')}}"
-                  alt=""
-                  data-bgposition="center top"
-                  data-bgfit="cover"
-                  data-bgrepeat="no-repeat" style="size:30%">
+              <li><img
+                  src="{{asset('assets/images/trafagoo.jpg')}}"
+                   style="size:10%"/>
               </li>
             </ul>
             <div class="tp-bannertimer"></div>
@@ -166,106 +131,3 @@
   <script src="{{asset('assets/js/wow.min.js')}}"></script>
   <script src="{{asset('assets/js/jquery.scrollspeed.js')}}"></script>
   <script src="{{asset('assets/js/main.js')}}"></script>
-	 <script>
-    (function($) {
-
-      "use strict";
-
-      //Main Slider
-      if($('.slider-apecsa .tp-banner').length){
-        jQuery('.slider-apecsa .tp-banner').show().revolution({
-          delay:1000,
-          startwidth:1,
-          startheight:310,
-          hideThumbs:300,
-
-          thumbWidth:80,
-          thumbHeight:50,
-          thumbAmount:5,
-
-          navigationType:"bullet",
-          navigationArrows:"0",
-          navigationStyle:"preview4",
-
-          touchenabled:"on",
-          onHoverStop:"off",
-
-          swipe_velocity: 0.7,
-          swipe_min_touches: 1,
-          swipe_max_touches: 1,
-          drag_block_vertical: false,
-
-          parallax:"mouse",
-          parallaxBgFreeze:"on",
-          parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
-
-          keyboardNavigation:"off",
-
-          navigationHAlign:"center",
-          navigationVAlign:"bottom",
-          navigationHOffset:0,
-          navigationVOffset:20,
-
-          soloArrowLeftHalign:"left",
-          soloArrowLeftValign:"center",
-          soloArrowLeftHOffset:20,
-          soloArrowLeftVOffset:0,
-
-          soloArrowRightHalign:"right",
-          soloArrowRightValign:"center",
-          soloArrowRightHOffset:20,
-          soloArrowRightVOffset:0,
-
-          shadow:0,
-          fullWidth:"on",
-          fullScreen:"off",
-
-          spinner:"spinner4",
-
-          stopLoop:"off",
-          stopAfterLoops:-1,
-          stopAtSlide:-1,
-
-          shuffle:"off",
-
-          autoHeight:"off",
-          forceFullWidth:"on",
-
-          hideThumbsOnMobile:"on",
-          hideNavDelayOnMobile:1500,
-          hideBulletsOnMobile:"on",
-          hideArrowsOnMobile:"on",
-          hideThumbsUnderResolution:0,
-
-          hideSliderAtLimit:0,
-          hideCaptionAtLimit:0,
-          hideAllCaptionAtLilmit:0,
-          startWithSlide:1,
-          videoJsPath:"",
-          fullScreenOffsetContainer: ".slider-apecsa"
-        });
-      }
-
-    })(window.jQuery);
-	
-    $(document).ready(function() {
-
-      var owl = $("#owl-slide-testimonial");
-     
-      owl.owlCarousel({
-          items : 100, //10 items above 1000px browser width
-          itemsDesktop : [401,100], //5 items between 1000px and 901px
-          itemsDesktopSmall : [301,100], // betweem 900px and 601px
-          itemsTablet: [300,100], //2 items between 600 and 0
-          itemsMobile : true // itemsMobile disabled - inherit from itemsTablet option
-      });
-
-      // Custom Navigation Events
-      $(".next").click(function(){
-        owl.trigger('owl.next');
-      })
-      $(".prev").click(function(){
-        owl.trigger('owl.prev');
-      })
-    });
-  </script>
