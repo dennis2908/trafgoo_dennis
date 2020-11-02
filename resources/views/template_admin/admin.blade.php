3e31fdab93b1893a831d0932d46b32f6486aad4f
dennis M <manullang_d@yahoo.com>
2020-11-02 20:13:08 +0700

    dmdmm

--- resources/views/template_admin/admin.blade.php
+++ resources/views/template_admin/admin.blade.php
@@ -32,7 +32,7 @@
 
 		.sidenav {
 		  height: 100%;
-		  width: 50px;
+		  width: 60px;
 		  position: fixed;
 		  z-index: 1;
 		  top: 0;
@@ -55,7 +55,7 @@
 		}
 
 		.main {
-		  margin-left: 50px; /* Same as the width of the sidenav */
+		  margin-left: 60px; /* Same as the width of the sidenav */
 		  font-size: 14px; /* Increased text to enable scrolling */
 		  padding: 0px 10px;
 		}
