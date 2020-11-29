6919f2b6349f89805115e73147fd201776741eab
dennis M <manullang_d@yahoo.com>
2020-11-29 15:48:57 +0700

    dmddm

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,53 +1,68 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #17a2b8;
-  height: 100vh;
-}
-#login .container #login-row #login-column #login-box {
-  margin-top: 120px;
-  max-width: 600px;
-  height: 320px;
-  border: 1px solid #9C9C9C;
-  background-color: #EAEAEA;
-}
-#login .container #login-row #login-column #login-box #login-form {
-  padding: 20px;
-}
-#login .container #login-row #login-column #login-box #login-form #register-link {
-  margin-top: -85px;
-}
-</style>
+<!DOCTYPE html>
+<html>
+<head>
+	
 
-<body>
-    <div id="login">
-       <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="{{route('dologin')}}" method="post">
+
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>T-Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="E-learning site for Students. Learn and Earn" />
+	<meta name="keywords" content="Start learning with a wide range of free online courses and develop your digital Knowledge, boost your CV with our courses" />
+	<meta name="author" content="Mike-Tech" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+ 
+	DESIGNED & DEVELOPED by Mike-Tech
+		
+	Website: 		http://Mikeih2253.com/
+	Email: 			chimikeih@gmail.com
+	Twitter: 		http://twitter.com/...
+	Facebook: 		https://www.facebook.com/f...
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<div id="center"><h1><i class="icon-study"></i>TRAFGOO</h1>
+<div id="center-set"> 
+<div id="signup">
+<div id="signup-st">
+<div align="center">
+<div id="reg-head" class="headrg">Contact Agent For Registration</div>
+</div>
+<h3 align="center" style="color: white"><a href="paymentgate.html"><h3 style="color: white"><button style="padding: 5px;padding-right: 60px; padding-left: 60px;"><b>Click to purchase your Pin</b></button></h3></a></h3>
+<div id="reg-bottom" class="btmrg">Learn & Earn</div>
+</div>
+</div>
+<div id="login">
+<div id="login-st">
+ <form id="login-form" class="form" action="{{route('dologin')}}" method="post">
 						@csrf
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control" autofocus required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="password" name="password" id="password" class="form-control" required>
-                            </div>
-							<div class="form-group">
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@include('modal') </body>
+<div id="reg-head" class="headrg">Login Here</div>
+<table border="0" align="center" cellpadding="2" cellspacing="0">
+<tr id="lg-1">
+<td class="tl-1"><div align="left" id="tb-name">Username:</div></td>
+<td><input type="text" id="tb-box" name="username" required /></td>
+</tr>
+<tr id="lg-1">
+<td class="tl-1"><div align="left" id="tb-name">Password:</div></td>
+<td><input id="tb-box" type="password" name="password" required /></td>
+</tr>
+</table>
+<div id="st"><input name="submit" type="submit" value="Login" id="st-btn"/></div>
+</form>
+<div id="reg-bottom" class="btmrg">Learn & Earn</div>
+</div>
+</div>
+</div>
+</div>
+<div id="footer"><p>&copy; 2020 Trafgoo.Edu. All Rights Reserved.</p></div>
+</body>
+</html>
